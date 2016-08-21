b9a7bbd0c87e2a052f92ed77644cfdb36fae25ed
Christopher A. Oliver <caowasteland@gmail.com>
2016-08-21 09:44:11 -0400

    Slight tweaks to lua debug shell.

--- shell.lua
+++ shell.lua
@@ -1,28 +1,30 @@
 -- Quick and dirty lua shell in lua.
 -- This may prove useful in testing/debugging.
 
-function lua_shell()
+function lua_shell(greet)
    local command, next_line, chunk, error
+   io.write((greet and greet..'/' or '')..'Lua shell:\n')
    repeat
       io.write('Lua> ')
-      next_line, command = io.read("*l"), ""
+      next_line, command = io.read '*l', ''
       if next_line then
-	 next_line, print_it = next_line:gsub("^=(.*)$", "return %1")
+	 next_line, print_it = next_line:gsub('^=(.*)$', 'return %1')
 	 repeat
-	    command = command..next_line.."\n"
-	    chunk, error = loadstring(command, "")
+	    command = command..next_line..'\n'
+	    chunk, error = loadstring(command, '')
 	    if chunk then
 	       (function (success, ...)
-		   if not success then error = ...
-		   elseif print_it ~= 0 then print(...)
-		   end end)(pcall(chunk))
+		     if not success then error = ...
+		     elseif print_it ~= 0 then print(...)
+	       end end)(pcall(chunk))
 	       break
 	    end
 	    if not error:find('<eof>') then break end
 	    io.write('>> ')
-	    next_line = io.read("*l")
+	    next_line = io.read '*l'
 	 until not next_line
-	 if error then print((error:gsub("^[^:]*:(.*)$", "stdin:%1"))) end
+	 if error then print((error:gsub('^[^:]*:(.*)$', 'stdin:%1'))) end
       end
    until not next_line
+   io.write 'Goodbye\n'
 end
